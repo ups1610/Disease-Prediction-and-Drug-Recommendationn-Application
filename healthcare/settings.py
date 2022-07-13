ddca276eee50c507cb778e39a03b4e9c0668c6d7
Upendra pratap singh <75423160+ups1610@users.noreply.github.com>
2022-07-13 22:58:35 +0530

    Update settings.py

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -15,7 +15,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_!z&k@5kr*n$2@^%c=-^-po*bj0&)3@6gdk&lkwg!qc&8_2l8^'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
